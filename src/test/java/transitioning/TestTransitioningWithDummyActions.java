da80df0979f38283ac8c39be92c8553630ff2435
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-03-16 18:05:57 +0100

    Made tests independent of each other removing order.

--- src/test/java/transitioning/TestTransitioningWithDummyActions.java
+++ src/test/java/transitioning/TestTransitioningWithDummyActions.java
@@ -17,13 +17,13 @@ import states.impl.*;
 @TestMethodOrder(OrderAnnotation.class)
 class TestTransitioningWithDummyActions {
 
-	private static StateMachine stateMachine;
-	private final static int dummyActionTime = 500;
+	private final static int dummyActionTime = 300;
+	private static IStateAction dummyAction;
 
 	@BeforeAll
 	static void setUp() {
 		// Create a dummy action that just pauses the thread
-		IStateAction dummyAction = new IStateAction() {
+		dummyAction = new IStateAction() {
 			@Override
 			public void execute() {
 				try {
@@ -34,111 +34,130 @@ class TestTransitioningWithDummyActions {
 			}
 		};
 
-		// setup a state machine with all actions being dummy actions
-		stateMachine = new StateMachineBuilder().withActionInStarting(dummyAction).withActionInExecute(dummyAction)
-				.withActionInCompleting(dummyAction).withActionInSuspending(dummyAction).withActionInUnsuspending(dummyAction)
-				.withActionInHolding(dummyAction).withActionInUnholding(dummyAction).withActionInAborting(dummyAction)
-				.withActionInClearing(dummyAction).withActionInStopping(dummyAction).withActionInResetting(dummyAction).build();
 	}
 
 	@Test
 	@Order(1)
-	void testSetup() {
+	void testSimpleSetup() {
+		StateMachine stateMachine = new StateMachineBuilder().build();
 		assertTrue(stateMachine.getState() instanceof IdleState, "After setup, state machine should be in IdleState");
 	}
 
 	@Test
 	@Order(2)
+	void testOtherInitialState() {
+		// Setup machine in stopped state
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new StoppedState()).build();
+		assertTrue(stateMachine.getState() instanceof StoppedState, "Machine should be in StoppedState");
+	}
+
+	@Test
 	void testAbortFromIdle() {
+		// Setup in any state, we just take suspended
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new SuspendedState()).withActionInAborting(dummyAction).build();
 		stateMachine.abort();
 		State currentState = stateMachine.getState();
 		assertTrue((currentState instanceof AbortingState), "Dummy action should lead to a delay, state machine should therefore be in Aborting");
 	}
 
 	@Test
-	@Order(3)
 	void waitForAbortedToBeReached() throws InterruptedException {
+		// Setup in any state, we just take complete
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new CompleteState()).withActionInAborting(dummyAction).build();
+		stateMachine.abort();
 		waitForDummyActionToBeCompleted(1); // Wait for aborting
 		assertTrue((stateMachine.getState() instanceof AbortedState),
 				"After the aborting action has been executed, Aborted should have been reached");
 	}
 
 	@Test
-	@Order(4)
 	void abortAgain() {
+		// Setup in Aborted State
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new AbortedState()).withActionInAborting(dummyAction).build();
 		stateMachine.abort();
 		assertTrue(stateMachine.getState() instanceof AbortedState, "State machine should stay in Aborted when abort is fired again");
 	}
 
 	@Test
-	@Order(5)
 	void testClearingWhenAborted() {
+		// Setup in Aborted State
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new AbortedState()).withActionInClearing(dummyAction).build();
 		stateMachine.clear();
 		assertTrue(stateMachine.getState() instanceof ClearingState, "Machine should switch to ClearingState when clearing is issued from aborted");
 	}
 
 	@Test
-	@Order(6)
 	void testAbortFromClearing() {
+		// Setup in Aborted State
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new AbortedState()).withActionInAborting(dummyAction)
+				.withActionInClearing(dummyAction).build();
+		stateMachine.clear();
 		stateMachine.abort();
 		assertTrue(stateMachine.getState() instanceof AbortingState, "Machine should switch to AbortingState when abort is fired while clearing ");
 	}
-	
+
 	@Test
-	@Order(7)
 	void testClearingAndResettingFromAborting() throws InterruptedException {
-		waitForDummyActionToBeCompleted(1); // wait for aborting
+		// Setup in Aborted State
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new AbortedState()).withActionInClearing(dummyAction)
+				.withActionInResetting(dummyAction).build();
 		stateMachine.clear();
 		waitForDummyActionToBeCompleted(1); // wait for clearing
 		stateMachine.reset();
 		waitForDummyActionToBeCompleted(1); // wait for resetting
 		assertTrue(stateMachine.getState() instanceof IdleState, "Machine should switch to IdleState when clearing and resetting from Aborted");
 	}
-	
+
 	@Test
-	@Order(8)
 	void testStartingFromIdle() throws InterruptedException {
+		// Setup in Idle State
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new IdleState()).withActionInStarting(dummyAction).build();
 		stateMachine.start();
 		assertTrue(stateMachine.getState() instanceof StartingState, "Machine should switch to StartingState when starting from Idle");
 	}
-	
-	
+
 	@Test
-	@Order(9)
 	void testStartingAndCompletingFromIdle() throws InterruptedException {
+		// Setup in Idle State
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new IdleState()).withActionInStarting(dummyAction)
+				.withActionInExecute(dummyAction).withActionInCompleting(dummyAction).build();
+		stateMachine.start();
 		waitForDummyActionToBeCompleted(3); // Wait for starting, executing and completing
-		assertTrue(stateMachine.getState() instanceof CompleteState, "Machine should switch to CompleteState after transitioning through Starting, Execute and Completing");
+		assertTrue(stateMachine.getState() instanceof CompleteState,
+				"Machine should switch to CompleteState after transitioning through Starting, Execute and Completing");
 	}
-	
+
 	@Test
-	@Order(10)
 	void testResettingToIdleFromComplete() throws InterruptedException {
+		// Setup in Complete State
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new CompleteState()).withActionInResetting(dummyAction).build();
 		stateMachine.reset();
 		waitForDummyActionToBeCompleted(1); // Wait for resetting
 		assertTrue(stateMachine.getState() instanceof IdleState, "Machine should switch to IdleState after transitioning through Resetting");
 	}
-	
+
 	@Test
-	@Order(11)
 	void testSuspendFromComplete() throws InterruptedException {
+		// Setup in Idle State
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new IdleState()).withActionInStarting(dummyAction)
+				.withActionInSuspending(dummyAction).withActionInExecute(dummyAction).build();
 		stateMachine.start();
 		waitForDummyActionToBeCompleted(1); // Wait for starting to be complete
 		stateMachine.suspend();
-		assertTrue(stateMachine.getState() instanceof SuspendingState, "Machine should switch to SuspendingState after when suspend is fired in ExecuteState");
+		assertTrue(stateMachine.getState() instanceof SuspendingState,
+				"Machine should switch to SuspendingState when suspend is fired in ExecuteState");
 	}
-	
-	
+
 	@Test
-	@Order(12)
 	void testUnsuspendFromSuspend() throws InterruptedException {
+		// Setup in Execute State
+		StateMachine stateMachine = new StateMachineBuilder().withInitialState(new ExecuteState()).withActionInSuspending(dummyAction).withActionInUnsuspending(dummyAction).build();
 		waitForDummyActionToBeCompleted(1); // Wait for suspending
 		stateMachine.unsuspend();
 		waitForDummyActionToBeCompleted(1); // Wait for unsuspending
 		assertTrue(stateMachine.getState() instanceof ExecuteState, "Machine should go back to Execute after unsuspend is fired in SuspendedState");
 	}
-	
-	
-	
+
 	private void waitForDummyActionToBeCompleted(int numberOfActionsToAwait) {
 		try {
 			Thread.sleep(numberOfActionsToAwait * dummyActionTime + 200);
@@ -146,6 +165,5 @@ class TestTransitioningWithDummyActions {
 			e.printStackTrace();
 		}
 	}
-	
 
 }
