1245f769249567002d80ed7ad02f844a32ba3e03
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-03-20 10:41:13 +0100

    Added test to test new generic withAction() function

---
+++ src/test/java/transitioning/TestStateMachineSetup.java
@@ -0,0 +1,38 @@
+package transitioning;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import statemachine.StateMachine;
+import statemachine.StateMachineBuilder;
+import states.ActiveStateName;
+import states.IStateAction;
+public class TestStateMachineSetup {
+
+	static IStateAction dummyAction;
+
+	@BeforeAll
+	static void setUp() {
+		dummyAction = new IStateAction() {
+			@Override
+			public void execute() {
+				// simply do nothing here
+			}
+		};
+
+	}
+
+	
+	@ParameterizedTest
+	@EnumSource(ActiveStateName.class)
+	void testActionSetup(ActiveStateName stateName) {
+		StateMachine stateMachine = new StateMachineBuilder().withAction(dummyAction, stateName).build();
+		IStateAction action = stateMachine.getStateActionManager().getAction(stateName);
+		assertEquals(action, dummyAction, "dummyAction should be added to state action");
+	}
+
+}
