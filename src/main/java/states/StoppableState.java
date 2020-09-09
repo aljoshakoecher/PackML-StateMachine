78c934602cd0299a378194ba8b945e3634de9aa6
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-09-09 19:09:28 +0200

    Fixed directly going to Stopped instead of  Stopping

--- src/main/java/states/StoppableState.java
+++ src/main/java/states/StoppableState.java
@@ -1,7 +1,7 @@
 package states;
 
 import statemachine.Isa88StateMachine;
-import states.impl.StoppedState;
+import states.impl.StoppingState;
 
 /**
  * Abstract super class for all states that can be stopped and aborted
@@ -14,6 +14,6 @@ public abstract class StoppableState extends AbortableState {
 		// Execute the stop-method that the developer implemented (has that to be executed in another thread?)
 		// Inside of the thread: Update the state in the ontology to now be "Stopping"
 		// When the thread is done: Update the state in the ontology to now be "Stopped" (might have to be done in the other thread as well)
-		stateMachine.setStateAndRunAction(new StoppedState());
+		stateMachine.setStateAndRunAction(new StoppingState());
 	}
 }
